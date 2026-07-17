ac9d354af47c43196cba78115fda7bcd21904175
enocben <benenoc5@gmail.com>
2026-07-17 16:03:56 +0200

    feat: add Shwary provider types

---
+++ app/providers/payment/shwary/types/index.ts
@@ -0,0 +1,11 @@
+export type WebhookResponse = {
+  id: string;
+  userId: string;
+  amount: number;
+  currency: string;
+  type: string;
+  status: string;
+  description: string;
+  recipientPhoneNumber: string;
+  referenceId: string
+}
