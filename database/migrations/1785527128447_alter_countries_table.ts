07850c5f1ec26d8cff0af723f58ae91f6eb62883
enocben <benenoc5@gmail.com>
2026-07-31 22:01:31 +0200

    fix: migration dropUnique avec CASCADE (FK dépendantes)

--- database/migrations/1785527128447_alter_countries_table.ts
+++ database/migrations/1785527128447_alter_countries_table.ts
@@ -4,9 +4,10 @@ export default class extends BaseSchema {
   protected tableName = 'countries'
 
   async up() {
-    this.schema.alterTable(this.tableName, (table) => {
-      table.dropUnique('code')
-    })
+    // CASCADE car des clés étrangères dépendent de cette contrainte unique
+    await this.db.rawQuery(
+      'ALTER TABLE "countries" DROP CONSTRAINT IF EXISTS "countries_code_unique" CASCADE'
+    )
   }
 
   async down() {
